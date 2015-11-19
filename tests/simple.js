bf35448eff5f4b43100e0cbc6d3d786c1d3d6b78
RaNKaR <esc.puf@gmail.com>
2015-11-19 19:10:52 +0200

    added tests

--- tests/simple.js
+++ tests/simple.js
@@ -14,4 +14,61 @@ describe("Something new", function() {
     it("contains not expectation", function() {
         expect(true).toBe(true);
     });
+
+    describe("Something new", function() {
+        it("contains spec with an expectation", function() {
+            expect(true).toBe(true);
+        });
+        it("contains not expectation", function() {
+            expect(true).toBe(true);
+        });
+    });
+});
+
+describe("A spec", function() {
+    var foo;
+
+    beforeEach(function() {
+        foo = 0;
+        foo += 1;
+    });
+
+    afterEach(function() {
+        foo = 0;
+    });
+
+    it("is just a function, so it can contain any code", function() {
+        expect(foo).toEqual(1);
+        pending('this is why it is pending');
+    });
+
+    it("can have more than one expectation", function() {
+        expect(foo).toEqual(1);
+        expect(true).toEqual(true);
+    });
+
+    describe("nested inside a second describe", function() {
+        var bar;
+
+        beforeEach(function() {
+            bar = 1;
+        });
+
+        it("can reference both scopes as needed", function() {
+            expect(foo).toEqual(bar);
+        });
+    });
+});
+
+xdescribe("A spec", function() {
+    var foo;
+
+    beforeEach(function() {
+        foo = 0;
+        foo += 1;
+    });
+
+    it("is just a function, so it can contain any code", function() {
+        expect(foo).toEqual(1);
+    });
 });
