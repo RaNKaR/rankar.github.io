7655739fb7c003a5ad04633a79e2068ff6d83d27
RaNKaR <esc.puf@gmail.com>
2015-11-19 16:04:44 +0200

    new reporter

--- my.conf.js
+++ my.conf.js
@@ -33,11 +33,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['tape'], //['progress'],
 
 
     // web server port
-    port: 9876,
+    port: 6666,
 
 
     // enable / disable colors in the output (reporters and logs)
