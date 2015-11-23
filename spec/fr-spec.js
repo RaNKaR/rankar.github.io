e3f04b05d1749490193c85467b365dff86fd7621
oleksandr_reshetnik@epam.com <oleksandr_reshetnik@epam.com>
2015-11-23 20:54:53 +0200

    env var

--- spec/fr-spec.js
+++
@@ -1,6 +0,0 @@
-var frisby = require('frisby');
-frisby.create('Twitter API v1 does not work.')
-  .get('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=brightbit')
-  .expectStatus(410)
-  .expectHeaderContains('content-type', 'application/json')
-  .toss();
